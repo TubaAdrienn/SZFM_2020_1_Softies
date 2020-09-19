7d8ba881c96dace764eab9d1c8201df5e1b21180
Tuba Adrienn <43565733+TubaAdrienn@users.noreply.github.com>
2020-09-19 12:37:04 +0200

    added method to get jokes from api

--- src/dao/fetchjokes.js
+++ src/dao/fetchjokes.js
@@ -20,5 +20,20 @@ class GetJokes {
         }
     }
 
-  
+    async getCategories() {
+        const response = await fetch("https://jokeapi-v2.p.rapidapi.com/categories?format=json", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "jokeapi-v2.p.rapidapi.com",
+                "x-rapidapi-key": "1434b5828dmsh57c108713d5e9efp191635jsnc07f3f8ab810"
+            }
+        });
+
+        const categories = await response.json();
+
+        return {
+            categories
+        }
+    }
+
 }
