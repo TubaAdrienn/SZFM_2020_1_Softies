8352b0a7b1a4623a5f4a0fd323454003cb98b1ea
fenyvesimate211 <43251500+fenyvesimate211@users.noreply.github.com>
2020-09-25 19:52:28 +0200

    Editing checkJokeParts

--- src/dao/util.js
+++ src/dao/util.js
@@ -2,11 +2,19 @@
 //static checkJokeParts - if joke.type==twopart put it together and return
 class Util{
     static checkJokeParts(joke){
-
+        if(joke.type=="twopart"){
+            let jokeToDisplay=joke.setup;
+            jokeToDisplay+=" ";
+            jokeToDisplay+=joke.delivery;
+            return jokeToDisplay;
+        }
+        else{
+            return joke.joke;
+        }
     }
 
     static memeOfTheDayGenerator(){
-        
+
     }
 }
 //static memeOfTheDayGenerator - Generate a number based on the date
