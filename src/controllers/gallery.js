1d8bae42ae6e24c9e0a29fc83868c996a8e7f57d
NRaccoon <43210244+NRaccoon@users.noreply.github.com>
2020-09-25 16:08:23 +0200

    Add event listener to gallery.js

--- src/controllers/gallery.js
+++ src/controllers/gallery.js
@@ -1,3 +1,13 @@
 //get a ui manipulator and a getter object
 // add eventlistener to DOMContentLoaded, fetchimgsrc
 // splitlines and setgallerypictures 
+
+var ui = new UI();
+var get = new GetJokes();
+
+document.addEventListener('DOMContentLoaded', () => {
+    get.fetchImgsSrc().then(resp => {
+        var srclines = resp.lines.split('\n');
+        ui.setGalleryPictures(srclines);
+    });
+});
