2f88391cbf474f81f7b92afeedc8d5cddfbcb1bd
fenyvesimate211 <43251500+fenyvesimate211@users.noreply.github.com>
2020-09-25 19:49:43 +0200

    Creating checkJokeParts and memeOfTheDayGenerator

--- src/dao/util.js
+++ src/dao/util.js
@@ -1,6 +1,12 @@
 //Util class:
 //static checkJokeParts - if joke.type==twopart put it together and return
 class Util{
-    
+    static checkJokeParts(joke){
+
+    }
+
+    static memeOfTheDayGenerator(){
+        
+    }
 }
 //static memeOfTheDayGenerator - Generate a number based on the date
